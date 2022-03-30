328176291fe732db9637b8db07bb2db1d9c57c53
Ladrillo <gabriel@cabrejas.net>
2022-03-30 19:52:48 +0200

    committing to "lecture"

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -3,12 +3,12 @@ import { Navigate } from 'react-router-dom'
 
 export default function Articles(props) {
   const {
-      articles,
-      getArticles,
-      deleteArticle,
-      updateArticle,
-      fetching,
-    } = props
+    articles,
+    getArticles,
+    deleteArticle,
+    updateArticle,
+    fetching,
+  } = props
 
   if (!window.localStorage.getItem('token')) {
     return <Navigate to="/" />
@@ -22,19 +22,21 @@ export default function Articles(props) {
     <div className="articles">
       <h2>Articles</h2>
       {
-        articles.map(art => {
-          return (
-            <div className="article" key={art.article_id}>
-              <div>
-                <h3>{art.title}</h3>
-                <p>{art.text}</p>
-                <p>topic: {art.topic}</p>
+        fetching
+          ? 'please wait...'
+          : articles.map(art => {
+            return (
+              <div className="article" key={art.article_id}>
+                <div>
+                  <h3>{art.title}</h3>
+                  <p>{art.text}</p>
+                  <p>topic: {art.topic}</p>
+                </div>
+                <button onClick={evt => deleteArticle(art.article_id)}>del</button>
+                <button onClick={evt => updateArticle(art.article_id)}>upd</button>
               </div>
-              <button onClick={evt => deleteArticle(art.article_id)}>del</button>
-              <button onClick={evt => updateArticle(art.article_id)}>upd</button>
-            </div>
-          )
-        })
+            )
+          })
       }
     </div>
   )
