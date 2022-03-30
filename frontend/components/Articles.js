af915c3dd2890cb728e5043a339cdfdc3ebd9f71
Ladrillo <gabriel@cabrejas.net>
2022-03-30 18:30:34 +0200

    committing to "lecture"

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -24,7 +24,7 @@ export default function Articles(props) {
                 <p>{art.text}</p>
                 <p>topic: {art.topic}</p>
               </div>
-              <button onClick={() => deleteArticle}>del</button>
+              <button onClick={() => deleteArticle(art.article_id)}>del</button>
             </div>
           )
         })
